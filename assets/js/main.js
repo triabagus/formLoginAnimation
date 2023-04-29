d4b1d00c5e6d98efde68dea4d89523baa1ac04f4
triabagus <triatop9@gmail.com>
2023-04-29 21:46:08 +0700

    Pass show hidden text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHidenPass =  (loginPass, loginEye) => {
+    const input = document.getElementById(loginPass),
+        iconEye = document.getElementById(loginEye)
+
+    iconEye.addEventListener('click', () => {
+        // Change password to text
+        if(input.type === 'password'){
+            // Switch
+            input.type = 'text'
+
+            // Icon Change
+            iconEye.classList.add('ri-eye-line')
+            iconEye.classList.remove('ri-eye-off-line')
+        }else{
+            // Change pass
+            input.type = 'password'
+
+            // Icon change
+            iconEye.classList.add("ri-eye-off-line");
+            iconEye.classList.remove("ri-eye-line");
+        }
+    })
+}
+
+showHidenPass('login-pass', 'login-eye')
